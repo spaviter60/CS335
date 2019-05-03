fb6a942cb6aa897df230ba3e3e0a1da96da632f6
spaviter60 <spaviter60@gmail.com>
2019-05-03 14:55:41 -0700

    Update wordcount.py

--- google-python-exercises/basic/wordcount.py
+++ google-python-exercises/basic/wordcount.py
@@ -6,37 +6,7 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
-"""Wordcount exercise
-Google's Python class
-
-The main() below is already defined and complete. It calls print_words()
-and print_top() functions which you write.
-
-1. For the --count flag, implement a print_words(filename) function that counts
-how often each word appears in the text and prints:
-word1 count1
-word2 count2
-...
-
-Print the above list in order sorted by word (python will sort punctuation to
-come before letters -- that's fine). Store all the words as lowercase,
-so 'The' and 'the' count as the same word.
-
-2. For the --topcount flag, implement a print_top(filename) which is similar
-to print_words() but which prints just the top 20 most common words sorted
-so the most common word is first, then the next most common, and so on.
-
-Use str.split() (no arguments) to split on all whitespace.
-
-Workflow: don't build the whole program at once. Get it to an intermediate
-milestone and print your data structure and sys.exit(0).
-When that's working, try for the next milestone.
-
-Optional: define a helper function to avoid code duplication inside
-print_words() and print_top().
-
-"""
-
+  
 import sys
 
 # +++your code here+++
@@ -46,6 +16,39 @@ import sys
 # Then print_words() and print_top() can just call the utility function.
 
 ###
+def sort(item):
+    return item[-1]
+
+def build(filename):
+    f = open(filename, 'rU')
+    words = f.read().split()
+    count = {}
+
+    for word in words:
+        word = word.lower()
+        if word not in count:
+            count[word] = 1
+        else:
+            count[word] += 1
+
+    f.close()
+
+    return count
+
+def print_words(filename):
+    dict = build(filename)
+
+    for word in sorted(dict.keys()):
+        print word, dict[word]
+
+def print_top(filename):
+    count = build(filename)
+    i = 0
+
+    items = sorted(count.items(), key=sort, reverse=True)
+    for item in items[:20]:
+        print item[0] + ': ' + str(item[1]) + ' times'
+        i += 1
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
